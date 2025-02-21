9e1c067663194e307641cb0016726a84c81d7198
Derrick Tran <derricktran2004@gmail.com>
2025-02-20 23:59:21 -0500

    Assignment Four Final

--- studentassignments/4/4-ShellP2/starter/dshlib.c
+++ studentassignments/4/4-ShellP2/starter/dshlib.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 #include "dshlib.h"
 
